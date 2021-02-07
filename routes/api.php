5dd6a7b7541a0c6b65af37b3ef4d589934870296
Isaac Oduh <ufuomaoduh@gmail.com>
2021-02-07 16:03:40 +0100

    Feat: User Setup, Profile, Passport

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,5 +16,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
+    Log::debug('User:', serialize($request->user()));
     return $request->user();
+});
+
+Route::get('version', function () {
+    return response()->json(['version' => config('app.version')]);
+});
+
+
+Route::namespace('App\\Http\\Controllers\\API')->group(function () {
+    Route::get('profile', 'ProfileController@profile');
+    Route::put('profile', 'ProfileController@updateProfile');
+    Route::post('change-password', 'ProfileController@changePassword');
 });
