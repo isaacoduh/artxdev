f99e0c6f75b07ea1179f771c8401c78bbef3ecd6
Isaac Oduh <ufuomaoduh@gmail.com>
2021-02-10 13:41:47 +0100

    chore: base view files setup

---
+++ resources/js/components/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+    <section class="content">
+        <h1>Dashboard</h1>
+    </section>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log("Component Mounted!");
+    }
+};
+</script>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('home', function () {
+    return redirect('/dashboard');
+});
+
+Route::get('/{vue_capture?}', function () {
+    return view('home');
+})->where('vue_capture', '[\/\w\.-]*')->middleware('auth');
