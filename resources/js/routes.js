e857a8c54dd0ff9aa26ec3cd0b2855fa350b0f41
Isaac Oduh <ufuomaoduh@gmail.com>
2021-02-10 16:31:07 +0100

    feat: Tag Frontend

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -2,5 +2,17 @@ export default [{
         path: "/dashboard",
         component: require("./components/Dashboard.vue").default
     },
+    {
+        path: "/products",
+        component: require("./components/product/Products.vue").default
+    },
+    {
+        path: "/product/tag",
+        component: require("./components/product/Tag.vue").default
+    },
+    {
+        path: "/product/category",
+        component: require("./components/product/Category.vue").default
+    },
     { path: "*", component: require("./components/NotFound.vue").default }
 ];
